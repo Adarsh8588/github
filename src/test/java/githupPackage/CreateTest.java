326fa0a3849ea9979fbe67c6b333611a838557fb
Smile <Smile@DESKTOP-08FKUO3>
2022-09-27 12:46:29 +0530

    commit

--- src/test/java/githupPackage/CreateTest.java
+++ src/test/java/githupPackage/CreateTest.java
@@ -1,1 +1,11 @@
 package githupPackage;
+
+import org.testng.annotations.Test;
+
+public class CreateTest{
+	@Test
+	
+	 public void create() {
+		 System.out.println("------created------");
+	 }
+ }
